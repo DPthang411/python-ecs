99e060690f016f7193740eadd1cc6e09e9c3be0e
DPthang411 <dinhphuocthang46@gmail.com>
2025-01-01 19:30:08 +0700

    Delete array directory

--- array/alternatelement.py
+++
@@ -1,6 +0,0 @@
-def alternate(array):
-    for index,e in enumerate(array, start=0):
-        if index % 2 == 0:
-            print(e)
-list = [1,2,3,4,5,6,7]
-alternate(list)
--- array/linearsearch.py
+++
@@ -1,7 +0,0 @@
-def linear_search(arr,target):
-    for i in  arr:
-        if i == target:
-            print(arr.index(i))
-arr = [1,2,3,5,7,43,65]
-tar = 3
-linear_search(arr, tar)
